469cb9ce6dc0772323e4198d3a884bfa47870225
lzq129 <451578541@qq.com>
2019-11-15 00:35:11 +0800

    task-7

--- src/task-7/app.vue
+++ src/task-7/app.vue
@@ -1,11 +1,52 @@
 <template>
-  <div class="result">结果展示</div>
+	<div>
+		<div class="result">结果展示</div>
+		<textarea v-model="text" v-show="!isSave"></textarea>
+		<span v-show="isSave">{{text}}</span>
+		<button @click="edit">编辑</button>
+		<button v-show="!isSave" @click="clear">清空</button>
+		<button v-show="!isSave" @click="preview">预览</button>
+		<button @click="save">保存</button>
+	</div>
 </template>
 <script>
-  console.log(1)
+	export default {//导出模块
+		data(){
+			return {
+				isSave: false,
+				text:''
+			}
+		},
+		// 实例化完成后，最早能操作data的函数
+		created(){
+			if(localStorage.getItem('text')!=null){
+				this.isSave=true,
+				this.text=localStorage.getItem('text')
+			}
+		},
+		// 自定义方法
+		methods:{
+			clear(){
+				this.text=''
+			},
+			preview(){
+				this.isSave = true
+			},
+			edit(){
+				this.isSave = false
+			},
+			save(){
+				this.isSave = true
+				// 本地储存，长期。setItem储存，getItem获取
+				localStorage.setItem('text',this.text)
+			}
+		}
+	}
 </script>
-<style>
-  .result{
-    color: red;
-  }
+<style lang="less">
+body{
+	.result{
+		color: red;
+	}
+}
 </style>
